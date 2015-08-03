f9eb9908008ffbee3728d83d9d2190f5a1d17919
Eyal Gruntman <gruntmane@janelia.hhmi.org>
2015-08-03 13:40:51 -0400

    plotting functions for AO and associated protcols

---
+++ Panel_Host_Control/plotStimStructAOResults.m
@@ -0,0 +1,64 @@
+function varargout = plotStimStructAOResults(pStruct)
+
+% function plotStimStructAOResults(pStruct)
+%
+% This function uses plotStimwAOResults to go through all the combinations
+% of the visual stimuli and the AO stim presented with them. 
+
+% Would require more work since timing issues are being sorted out
+%
+% Note AO stim alone is refernced as 0 and not length(uniStim)
+%
+% OUTPUT
+% variable:
+% one output    -       cell array of axes handles
+% two outputs   -       cellarray of axes handles and vector of figure
+%                       handles
+
+close all
+
+assert(isfield(pStruct, 'uniStim'), 'protocol structure is missing uniStim field')
+
+uniStimInd = 1:length(pStruct.uniStim);
+uniStimInd(end) = 0;
+fh = zeros(1, length(uniStimInd));
+axhCell = cell(1, length(uniStimInd));
+startPos = [0.4 0.3 0.2 0.4];
+
+for ii=1:length(uniStimInd)
+    
+    fh(ii) = figure('units', 'normalized', 'position', startPos, ...
+                    'NumberTitle', 'off', 'Name', num2str(pStruct.uniStim(ii).relInds));
+    axhCell{ii} = plotStimwAOResults(pStruct, uniStimInd(ii), fh(ii));
+    
+    if ii==1
+        beep
+        foo = input('Resize figure window and press any key \n');
+        startPos = get(fh(1), 'position');
+    end
+    
+end
+
+allYLim = cellfun(@(z) arrayfun(@(x) get(x, 'ylim'),  z, 'uniformoutput', 0), axhCell, 'uniformoutput', 0);
+allYLim2 = cellfun(@(x) vertcat(x{:}), allYLim, 'uniformoutput', 0);
+allYLim2 = vertcat(allYLim2{:});
+yyMax = max(allYLim2(:,2));
+yyMin = min(allYLim2(:,1));
+
+for ii=1:length(axhCell)
+    tempAx = axhCell{ii};
+    set(tempAx(:), 'ylim', [yyMin, yyMax])
+end
+    
+
+switch nargout
+    case 1
+        varargout{1} = axhCell;
+    case 2
+        varargout{1} = axhCell;
+        varargout{2} = fh;
+end
+
+
+
+end
--- Panel_Host_Control/plotStimStructResults.m
+++ Panel_Host_Control/plotStimStructResults.m
@@ -64,7 +64,7 @@ for ii=1:numFigs
             plotInds = find(firstInds+secInds+thirdInds+dataInd == 4);
             plotCol = relCols(goodColsInds(kk), :);
             for mm=1:length(plotInds)
-                dataX = pStruct.stim(plotInds(mm)).data{1}(1, :); 
+                dataX = pStruct.stim(plotInds(mm)).data{1}(:, 1); 
                 dataX = dataX-dataX(1); % gets rid of samples that doen't start w/ zero
                 if dataX(1) < minX
                     minX = dataX(1);
@@ -72,7 +72,7 @@ for ii=1:numFigs
                 if dataX(end) > maxX
                     maxX = dataX(end);
                 end
-                dataY = pStruct.stim(plotInds(mm)).data{1}(relChannel, :)*10; % to convert to mV
+                dataY = pStruct.stim(plotInds(mm)).data{1}(:, relChannel)*10; % to convert to mV
                 lh = plot(axh(ii, jj), dataX, dataY, 'linewidth', 1, 'color', plotCol);
             end
             if ~isempty(plotInds)
